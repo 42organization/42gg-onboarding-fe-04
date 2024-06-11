fe6639520d8582f03100963cdfa4ce8e807e8ce9
yhames <wjddn494@naver.com>
2024-06-11 15:00:25 +0900

    docs: fix typos

--- jeongwpa/init.sql
+++ jeongwpa/init.sql
@@ -1,4 +1,3 @@
-------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `student` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
@@ -10,9 +9,9 @@ CREATE TABLE IF NOT EXISTS `student` (
   `updated_by` DATETIME NOT NULL,
   CONSTRAINT `unique_student_name_birth_date` UNIQUE (`name`, `birth_date`)
 );
+
 CREATE INDEX IF NOT EXISTS `student_name_birth_date_index` ON `student` (`name`, `birth_date`);
-------------------------------------------------------------
-------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `course` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
@@ -25,8 +24,7 @@ CREATE TABLE IF NOT EXISTS `course` (
   `updated_by` DATETIME NOT NULL,
   CONSTRAINT `unique_course_name_professor_name_credit` UNIQUE (`name`, `professor_name`, `credit`)
 );
-------------------------------------------------------------
-------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `student_course` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
   `course_id` INTEGER NOT NULL,
@@ -37,4 +35,3 @@ CREATE TABLE IF NOT EXISTS `student_course` (
   CONSTRAINT `fk_student_course_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`),
   CONSTRAINT `fk_student_course_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`)
 );
-------------------------------------------------------------
