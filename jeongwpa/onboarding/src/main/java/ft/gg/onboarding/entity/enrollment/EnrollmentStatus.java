8c00274dbf06ec6a98b4dc712a46a9d44fdede90
yhames <wjddn494@naver.com>
2024-06-13 17:41:44 +0900

    feat: fix enum

--- jeongwpa/onboarding/src/main/java/ft/gg/onboarding/entity/enrollment/EnrollmentStatus.java
+++ jeongwpa/onboarding/src/main/java/ft/gg/onboarding/entity/enrollment/EnrollmentStatus.java
@@ -3,13 +3,6 @@ package ft.gg.onboarding.entity.enrollment;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
 public enum EnrollmentStatus {
-
-    ENROLL("enroll"),
-    CANCEL("drop"),
-    SUCCESS("success");
-
-    private final String value;
+    ENROLL, CANCEL, SUCCESS;
 }
--- jeongwpa/onboarding/src/main/java/ft/gg/onboarding/entity/student/StudentStatus.java
+++ jeongwpa/onboarding/src/main/java/ft/gg/onboarding/entity/student/StudentStatus.java
@@ -1,15 +1,5 @@
 package ft.gg.onboarding.entity.student;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum StudentStatus {
-
-    ATTEND("attend"),
-    DROP("drop"),
-    GRADUATE("graduate");
-
-    private final String value;
+    ATTEND, DROP, GRADUATE;
 }
