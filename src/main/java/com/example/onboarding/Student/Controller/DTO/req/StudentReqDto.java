029ce715143c5a74b66bd89ea34030bd6a979c7b
WONIE <wonie@WONIEui-MacBookPro.local>
2024-11-26 12:34:34 +0900

    ignore

--- src/main/java/com/example/onboarding/Student/Controller/DTO/req/StudentReqDto.java
+++ src/main/java/com/example/onboarding/Student/Controller/DTO/req/StudentReqDto.java
@@ -1,4 +1,15 @@
 package com.example.onboarding.Student.Controller.DTO.req;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentReqDto {
+	private String studentName;
+	private String
 }
