7e5e5d359f2ba417da41f0e1ac32022696179849
seykim <kksy0917@gmail.com>
2024-11-26 12:33:40 +0900

    [Feat] 온보딩 8~9일차 API 작성 - seykim

---
+++ seykim/Onboarding/src/main/java/com/sample/test/onboarding/Course/Controller/Dto/Response/CourseResDto.java
@@ -0,0 +1,4 @@
+package com.sample.test.onboarding.Course.Controller.Dto.Response;
+
+public class CourseResDto {
+}
---
+++ seykim/Onboarding/src/main/java/com/sample/test/onboarding/Data/Status/StudentCourseStatus.java
@@ -0,0 +1,5 @@
+package com.sample.test.onboarding.Data.Status;
+
+public enum StudentCourseStatus {
+	ONGOING, CANCEL, COMPLETED
+}
