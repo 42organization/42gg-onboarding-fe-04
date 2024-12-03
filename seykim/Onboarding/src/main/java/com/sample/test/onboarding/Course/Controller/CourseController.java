00ad3edfababbe6f58e0c518ee66f6d92238fd2d
seykim <kksy0917@gmail.com>
2024-12-03 17:58:00 +0900

    [Feat] 코드 리뷰 내용 반영 및 수정

--- seykim/Onboarding/src/main/java/com/sample/test/onboarding/Course/Controller/CourseController.java
+++ seykim/Onboarding/src/main/java/com/sample/test/onboarding/Course/Controller/CourseController.java
@@ -2,7 +2,6 @@ package com.sample.test.onboarding.Course.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
