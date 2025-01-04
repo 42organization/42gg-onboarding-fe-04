77f8fba40c33d484c39f4697f7689d53d057fc92
wonie <wonies@github.com>
2025-01-04 21:51:50 +0900

    [Refactoring] 바뀐 로직

---
+++ wonhshin/Onboarding/src/main/java/com/example/onboarding/course/controller/dto/req/CourseUpdateReqDto.java
@@ -0,0 +1,12 @@
+// package com.example.onboarding.course.controller.dto.req;
+//
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+//
+// @Getter
+// @AllArgsConstructor
+// @NoArgsConstructor
+// public class CourseUpdateReqDto {
+//
+// }
